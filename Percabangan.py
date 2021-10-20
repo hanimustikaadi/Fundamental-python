6b917ff722d89a46daee01ae6b244c14f3cdecfa
hani <mustikahani@yahoo.com>
2021-10-21 00:04:25 +0700

    initial commit

---
+++ Percabangan.py
@@ -0,0 +1,10 @@
+jumlah_buku = 10
+print('Ibu berkata: "baca semua bukumu".')
+
+Jumlah_buku_sudah_dibaca= 0
+print(f'Jumlah buku yang sudah di baca {Jumlah_buku_sudah_dibaca}')
+
+for Jumlah_buku_sudah_dibaca in range(1,jumlah_buku +1 ):
+    print(f'buku ke {Jumlah_buku_sudah_dibaca} sudah di baca')
+
+print(f' jumlah buku sudah dibaca {Jumlah_buku_sudah_dibaca}')
---
+++ PythonPercabanganWhile.py
@@ -0,0 +1,11 @@
+jumlah_buku = 10
+print('Ibu berkata: "baca semua bukumu".')
+
+Jumlah_buku_sudah_dibaca= 0
+print(f'Jumlah buku yang sudah di baca {Jumlah_buku_sudah_dibaca}')
+
+while Jumlah_buku_sudah_dibaca < jumlah_buku :
+    Jumlah_buku_sudah_dibaca = Jumlah_buku_sudah_dibaca + 1
+    print(f'buku ke {Jumlah_buku_sudah_dibaca} sudah di baca')
+
+print(f' jumlah buku sudah dibaca {Jumlah_buku_sudah_dibaca}')
